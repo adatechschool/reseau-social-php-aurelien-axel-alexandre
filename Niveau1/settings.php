1eeff0020b131028910fae8d017c8294b6bd5ea7
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-02-08 15:25:18 +0100

    settings.php real finish

--- Niveau1/settings.php
+++ Niveau1/settings.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="fr">
+
     <head>
         <meta charset="utf-8">
         <title>ReSoC - Paramètres</title>
@@ -7,38 +8,7 @@
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-
-            </nav>
-        </header>
-        <div id="wrapper" class='profile'>
-
-
-            <aside>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez les informations de l'utilisatrice
-                        n° <?php $userId ?></p>
-
-                </section>
-            </aside>
-            <main>
-                <?php
+    <?php
                 /**
                  * Etape 1: Les paramètres concernent une utilisatrice en particulier
                  * La première étape est donc de trouver quel est l'id de l'utilisatrice
@@ -80,8 +50,39 @@
                  * Etape 4: à vous de jouer
                  */
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer les valeurs ci-après puiseffacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "coucou" . "</pre>";
+                //echo "<pre>" . print_r($user, 1) . "coucou" . "</pre>";
                 ?>
+        <header>
+            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
+            <nav id="menu">
+                <a href="news.php">Actualités</a>
+                <a href="wall.php?user_id=<?php echo $user['id']; ?>">Mur</a>
+                <a href="feed.php?user_id=<?php echo $user['id']; ?>">Flux</a>
+                <a href="tags.php?tag_id=1">Mots-clés</a>
+            </nav>
+            <nav id="user">
+                <a href="#">Profil</a>
+                <ul>
+                    <li><a href="settings.php?user_id=<?php echo $user['id']; ?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $user['id']; ?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $user['id']; ?>">Mes abonnements</a></li>
+                </ul>
+
+            </nav>
+        </header>
+        <div id="wrapper" class='profile'>
+
+
+            <aside>
+                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <section>
+                    <h3>Présentation</h3>
+                    <p>Sur cette page vous trouverez les informations de l'utilisatrice
+                        n° <?php echo $userId ?></p>
+
+                </section>
+            </aside>
+            <main>
                 <article class='parameters'>
                     <h3>Mes paramètres</h3>
                     <dl>
