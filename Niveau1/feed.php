cbf519341422dc4d32842e8b33b79646261c5d4d
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-02-08 16:45:49 +0100

    feed en cours

--- Niveau1/feed.php
+++ Niveau1/feed.php
@@ -58,7 +58,7 @@
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message des utilisatrices
+                    <p>Sur cette page vous trouverez tous les message des utilisatrices : <?php echo $user['alias'] ?>
                         auxquel est abonnée l'utilisatrice XXX
                         (n° <?php echo $userId ?>)
                     </p>
@@ -96,26 +96,32 @@
                  * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
                  * A vous de retrouver comment faire la boucle while de parcours...
                  */
+                while ($post = $lesInformations->fetch_assoc())
+                {
                 ?>
+
+
                 <article>
-                    <h3>
-                        <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
-                    </h3>
-                    <address>par AreTirer</address>
-                    <div>
-                        <p>Ceci est un paragraphe</p>
-                        <p>Ceci est un autre paragraphe</p>
-                        <p>... de toutes manières il faut supprimer cet
-                            article et le remplacer par des informations en
-                            provenance de la base de donnée</p>
-                    </div>
-                    <footer>
-                        <small>♥ 132</small>
-                        <a href="">#lorem</a>,
-                        <a href="">#piscitur</a>,
-                    </footer>
-                </article>
+                        <h3>
+                            <time><?php echo $post['created'] ?></time>
+                        </h3>
+                        <address><?php echo 'par ' . $post['author_name'] ?></address>
+                        <div>
+                            <p><?php echo $post['content'] ?></p>
+                        </div>
+                        <footer>
+                            <small><?php echo '♥' . $post['like_number'] ?></small>
+
+                            <?php
+                                $tags = explode(',', $post['taglist']);
+                                foreach ($tags as $tag) {
+                                    echo '<a href="#">#' . trim($tag) . '</a> ';
+                                }
+                            ?>
+                        </footer>
+                    </article>
                 <?php
+                }
                 // et de pas oublier de fermer ici vote while
                 ?>
 
