dd64de3365f035e75b3666443c7080625c45aeca
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-02-08 15:40:09 +0100

    wall.php ok

--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -27,7 +27,7 @@
         </header>
         <div id="wrapper">
             <?php
-            /**
+            /*
              * Etape 1: Le mur concerne un utilisateur en particulier
              * La première étape est donc de trouver quel est l'id de l'utilisateur
              * Celui ci est indiqué en parametre GET de la page sous la forme user_id=...
@@ -37,7 +37,7 @@
             $userId =intval($_GET['user_id']);
             ?>
             <?php
-            /**
+            /*
              * Etape 2: se connecter à la base de donnée
              */
             $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
@@ -45,26 +45,26 @@
 
             <aside>
                 <?php
-                /**
+                /*
                  * Etape 3: récupérer le nom de l'utilisateur
                  */
                 $laQuestionEnSql = "SELECT * FROM users WHERE id= '$userId' ";
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $user = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
+                //todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
+                // echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : XXX
+                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'] ?>
                         (n° <?php echo $userId ?>)
                     </p>
                 </section>
             </aside>
             <main>
                 <?php
-                /**
+                /*
                  * Etape 3: récupérer tous les messages de l'utilisatrice
                  */
                 $laQuestionEnSql = "
@@ -91,29 +91,30 @@
                 while ($post = $lesInformations->fetch_assoc())
                 {
 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    // echo "<pre>" . print_r($post, 1) . "</pre>";
                     ?>
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address><?php echo 'par ' . $post['author_name'] ?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet
-                                article et le remplacer par des informations en
-                                provenance de la base de donnée</p>
+                            <p><?php echo $post['content'] ?></p>
                         </div>
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small><?php echo '♥' . $post['like_number'] ?></small>
+
+                            <?php
+                                $tags = explode(',', $post['taglist']);
+                                foreach ($tags as $tag) {
+                                    echo '<a href="#">#' . trim($tag) . '</a> ';
+                                }
+                            ?>
                         </footer>
                     </article>
-                <?php } ?>
-
-
+                <?php
+            }
+            ?>
             </main>
         </div>
     </body>
