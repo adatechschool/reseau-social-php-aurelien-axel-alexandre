203181ccbf403d29db52a259b7752a1423f5d2c3
axelchicheportiche <140517984+axelchicheportiche@users.noreply.github.com>
2024-02-08 15:57:40 +0100

    wall.php OK

--- Niveau1/wall.php
+++ Niveau1/wall.php
@@ -7,26 +7,7 @@
         <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
-
-            </nav>
-        </header>
-        <div id="wrapper">
-            <?php
+    <?php
             /*
              * Etape 1: Le mur concerne un utilisateur en particulier
              * La première étape est donc de trouver quel est l'id de l'utilisateur
@@ -42,9 +23,7 @@
              */
             $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
             ?>
-
-            <aside>
-                <?php
+            <?php
                 /*
                  * Etape 3: récupérer le nom de l'utilisateur
                  */
@@ -54,17 +33,7 @@
                 //todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
                 // echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'] ?>
-                        (n° <?php echo $userId ?>)
-                    </p>
-                </section>
-            </aside>
-            <main>
-                <?php
-                /*
+                <?php /*
                  * Etape 3: récupérer tous les messages de l'utilisatrice
                  */
                 $laQuestionEnSql = "
@@ -83,7 +52,50 @@
                 if ( ! $lesInformations)
                 {
                     echo("Échec de la requete : " . $mysqli->error);
-                }
+                } ?>
+
+
+
+
+
+        <header>
+            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
+            <nav id="menu">
+                <a href="news.php">Actualités</a>
+                <a href="wall.php?user_id=<?php echo $user['id']?>">Mur</a>
+                <a href="feed.php?user_id=<?php echo $user['id']?>">Flux</a>
+                <a href="tags.php?tag_id=1">Mots-clés</a>
+            </nav>
+            <nav id="user">
+                <a href="#">Profil</a>
+                <ul>
+                    <li><a href="settings.php?user_id=<?php echo $user['id']?>">Paramètres</a></li>
+                    <li><a href="followers.php?user_id=<?php echo $user['id']?>">Mes suiveurs</a></li>
+                    <li><a href="subscriptions.php?user_id=<?php echo $user['id']?>">Mes abonnements</a></li>
+                </ul>
+
+            </nav>
+        </header>
+        <div id="wrapper">
+
+
+            <aside>
+
+                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
+                <section>
+                    <h3>Présentation</h3>
+                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo $user['alias'] ?>
+                        (n° <?php echo $userId ?>)
+                    </p>
+                </section>
+            </aside>
+
+
+
+
+
+            <main>
+                <?php
 
                 /*
                  * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
@@ -113,7 +125,7 @@
                         </footer>
                     </article>
                 <?php
-            }
+                }
             ?>
             </main>
         </div>
