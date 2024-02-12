3a7a135711d6e1cf24ed0bb1f71b2f91ef2041ee
“Aurélien <aureliencartier@gmail.com>
2024-02-12 10:43:26 +0100

    Finaliastion 1er partie fichier feed.php, tags.php, subscriptions.php, followers.php

--- Niveau1/feed.php
+++ Niveau1/feed.php
@@ -1,132 +1,156 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Flux</title>
-        <meta name="author" content="TripleA">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
 
-            </nav>
-        </header>
-        <div id="wrapper">
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Flux</title>
+    <meta name="author" content="TripleA">
+    <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+    <?php
+    /**
+     * Cette page est TRES similaire à wall.php.
+     * Vous avez sensiblement à y faire la meme chose.
+     * Il y a un seul point qui change c'est la requete sql.
+     */
+    /**
+     * Etape 1: Le mur concerne un utilisateur en particulier
+     */
+    $userId = intval($_GET['user_id']);
+    ?>
+    <?php
+    /*
+    * Etape 2: se connecter à la base de donnée
+    */
+    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+
+    if ($mysqli->connect_errno) {
+        echo "<article>";
+        echo ("Échec de la connexion : " . $mysqli->connect_error);
+        echo ("<p>Indice: Vérifiez les parametres de <code>new mysqli(...</code></p>");
+        echo "</article>";
+        exit();
+    }
+
+    //Etape 3: récupérer le nom de l'utilisateur
+    $laQuestionEnSqlUser = "SELECT * FROM users WHERE id= '$userId' ";
+    $lesInformationsUser = $mysqli->query($laQuestionEnSqlUser);
+    $user = $lesInformationsUser->fetch_assoc();
+
+    //echo "<pre>" . print_r($user, 1) . "</pre>";
+
+    /**
+     * Etape 3: récupérer tous les messages des abonnements
+     */
+    $laQuestionEnSqlMessage = "
+                SELECT posts.content,
+                posts.created,
+                users.alias as author_name,
+                count(likes.id) as like_number,
+                GROUP_CONCAT(DISTINCT tags.label) AS taglist
+                FROM followers
+                JOIN users ON users.id=followers.followed_user_id
+                JOIN posts ON posts.user_id=users.id
+                LEFT JOIN posts_tags ON posts.id = posts_tags.post_id
+                LEFT JOIN tags       ON posts_tags.tag_id  = tags.id
+                LEFT JOIN likes      ON likes.post_id  = posts.id
+                WHERE followers.following_user_id='$userId'
+                GROUP BY posts.id
+                ORDER BY posts.created DESC
+                ";
+    $lesInformationsMessage = $mysqli->query($laQuestionEnSqlMessage);
+
+    /* 
+    echo "<pre>";
+    print_r($lesInformationsMessage->fetch_all(MYSQLI_ASSOC));
+    echo "</pre>";
+    */
+
+    $messages = [];
+
+    while ($post = $lesInformationsMessage->fetch_assoc()) {
+        $messages[] = $post;
+    }
+
+    $followersAlias = [];
+
+    foreach ($messages as $post) {
+        $followersAlias[] = $post['author_name'];
+    }
+
+    $followersAlias = array_unique($followersAlias);
+
+    $followersList = implode(', ', $followersAlias);
+    ?>
+
+    <header>
+        <img src="resoc.jpg" alt="Logo de notre réseau social" />
+        <nav id="menu">
+            <a href="news.php">Actualités</a>
+            <a href="wall.php?user_id=<?php echo $user['id'] ?>">Mur</a>
+            <a href="feed.php?user_id=<?php echo $user['id'] ?>">Flux</a>
+            <a href="tags.php?tag_id=1">Mots-clés</a>
+        </nav>
+        <nav id="user">
+            <a href="#">Profil</a>
+            <ul>
+                <li><a href="settings.php?user_id=<?php echo $user['id'] ?>">Paramètres</a></li>
+                <li><a href="followers.php?user_id=<?php echo $user['id'] ?>">Mes suiveurs</a></li>
+                <li><a href="subscriptions.php?user_id=<?php echo $user['id'] ?>">Mes abonnements</a></li>
+            </ul>
+
+        </nav>
+    </header>
+
+    <div id="wrapper">
+        <aside>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <section>
+                <!-- todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous -->
+                <h3>Présentation</h3>
+                <p>Sur cette page vous trouverez tous les message des utilisatrices : <?php echo $followersList ?><br><br>
+                    Auxquel est abonnée l'utilisatrice <?php echo $user['alias'] ?>
+                    (n° <?php echo $userId ?>)
+                </p>
+
+            </section>
+        </aside>
+
+        <main>
             <?php
             /**
-             * Cette page est TRES similaire à wall.php.
-             * Vous avez sensiblement à y faire la meme chose.
-             * Il y a un seul point qui change c'est la requete sql.
+             * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
+             * A vous de retrouver comment faire la boucle while de parcours...
              */
-            /**
-             * Etape 1: Le mur concerne un utilisateur en particulier
-             */
-            $userId = intval($_GET['user_id']);
+            foreach ($messages as $post) {
             ?>
-            <?php
-            /**
-             * Etape 2: se connecter à la base de donnée
-             */
-            $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-            ?>
-
-            <aside>
-                <?php
-                /**
-                 * Etape 3: récupérer le nom de l'utilisateur
-                 */
-                $laQuestionEnSql = "SELECT * FROM `users` WHERE id= '$userId' ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                $user = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($user, 1) . "</pre>";
-                ?>
-                <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message des utilisatrices : <?php echo $user['alias'] ?>
-                        auxquel est abonnée l'utilisatrice XXX
-                        (n° <?php echo $userId ?>)
-                    </p>
-
-                </section>
-            </aside>
-            <main>
-                <?php
-                /**
-                 * Etape 3: récupérer tous les messages des abonnements
-                 */
-                $laQuestionEnSql = "
-                    SELECT posts.content,
-                    posts.created,
-                    users.alias as author_name,
-                    count(likes.id) as like_number,
-                    GROUP_CONCAT(DISTINCT tags.label) AS taglist
-                    FROM followers
-                    JOIN users ON users.id=followers.followed_user_id
-                    JOIN posts ON posts.user_id=users.id
-                    LEFT JOIN posts_tags ON posts.id = posts_tags.post_id
-                    LEFT JOIN tags       ON posts_tags.tag_id  = tags.id
-                    LEFT JOIN likes      ON likes.post_id  = posts.id
-                    WHERE followers.following_user_id='$userId'
-                    GROUP BY posts.id
-                    ORDER BY posts.created DESC
-                    ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                if ( ! $lesInformations)
-                {
-                    echo("Échec de la requete : " . $mysqli->error);
-                }
-
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 * A vous de retrouver comment faire la boucle while de parcours...
-                 */
-                while ($post = $lesInformations->fetch_assoc())
-                {
-                ?>
-
-
                 <article>
-                        <h3>
-                            <time><?php echo $post['created'] ?></time>
-                        </h3>
-                        <address><?php echo 'par ' . $post['author_name'] ?></address>
-                        <div>
-                            <p><?php echo $post['content'] ?></p>
-                        </div>
-                        <footer>
-                            <small><?php echo '♥' . $post['like_number'] ?></small>
+                    <h3>
+                        <time><?php echo $post['created'] ?></time>
+                    </h3>
+                    <address><?php echo 'par ' . $post['author_name'] ?></address>
+                    <div>
+                        <p><?php echo $post['content'] ?></p>
+                    </div>
+                    <footer>
+                        <small><?php echo '♥' . $post['like_number'] ?></small>
 
-                            <?php
-                                $tags = explode(',', $post['taglist']);
-                                foreach ($tags as $tag) {
-                                    echo '<a href="#">#' . trim($tag) . '</a> ';
-                                }
-                            ?>
-                        </footer>
-                    </article>
-                <?php
-                }
-                // et de pas oublier de fermer ici vote while
-                ?>
+                        <?php
+                        $tags = explode(',', $post['taglist']);
+                        foreach ($tags as $tag) {
+                            echo '<a href="#">#' . trim($tag) . '</a> ';
+                        }
+                        ?>
+                    </footer>
+                </article>
+            <?php
+            }
+            // et de pas oublier de fermer ici vote while
+            ?>
+        </main>
+    </div>
+</body>
 
-
-            </main>
-        </div>
-    </body>
 </html>
--- Niveau1/followers.php
+++ Niveau1/followers.php
@@ -1,65 +1,101 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Mes abonnés </title>
-        <meta name="author" content="TripleA">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-        <header>
-            <img src="resoc.jpg" alt="Logo de notre réseau social"/>
-            <nav id="menu">
-                <a href="news.php">Actualités</a>
-                <a href="wall.php?user_id=5">Mur</a>
-                <a href="feed.php?user_id=5">Flux</a>
-                <a href="tags.php?tag_id=1">Mots-clés</a>
-            </nav>
-            <nav id="user">
-                <a href="#">Profil</a>
-                <ul>
-                    <li><a href="settings.php?user_id=5">Paramètres</a></li>
-                    <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
-                    <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
-                </ul>
 
-            </nav>
-        </header>
-        <div id="wrapper">
-            <aside>
-                <img src = "user.jpg" alt = "Portrait de l'utilisatrice"/>
-                <section>
-                    <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez la liste des personnes qui
-                        suivent les messages de l'utilisatrice
-                        n° <?php echo intval($_GET['user_id']) ?></p>
+<head>
+    <meta charset="utf-8">
+    <title>ReSoC - Mes abonnés </title>
+    <meta name="author" content="TripleA">
+    <link rel="stylesheet" href="style.css" />
+</head>
 
-                </section>
-            </aside>
-            <main class='contacts'>
-                <?php
-                // Etape 1: récupérer l'id de l'utilisateur
-                $userId = intval($_GET['user_id']);
-                // Etape 2: se connecter à la base de donnée
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
-                // Etape 3: récupérer le nom de l'utilisateur
-                $laQuestionEnSql = "
-                    SELECT users.*
-                    FROM followers
-                    LEFT JOIN users ON users.id=followers.following_user_id
-                    WHERE followers.followed_user_id='$userId'
-                    GROUP BY users.id
-                    ";
-                $lesInformations = $mysqli->query($laQuestionEnSql);
-                // Etape 4: à vous de jouer
-                //@todo: faire la boucle while de parcours des abonnés et mettre les bonnes valeurs ci dessous
-                ?>
-                <article>
-                    <img src="user.jpg" alt="blason"/>
-                    <h3>Béatrice</h3>
-                    <p>id:321</p>
-                </article>
-            </main>
-        </div>
-    </body>
+<body>
+
+    <?php
+    // Etape 1: récupérer l'id de l'utilisateur
+    $userId = intval($_GET['user_id']);
+
+    // Etape 2: se connecter à la base de donnée
+    $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+
+    if ($mysqli->connect_errno) {
+        echo "<article>";
+        echo ("Échec de la connexion : " . $mysqli->connect_error);
+        echo ("<p>Indice: Vérifiez les parametres de <code>new mysqli(...</code></p>");
+        echo "</article>";
+        exit();
+    }
+
+    // Etape 3: récupérer id et nom de l'utilisateur
+    $laQuestionEnSqlUser = "SELECT * FROM users WHERE id= '$userId' ";
+    $lesInformationsUser = $mysqli->query($laQuestionEnSqlUser);
+    $user = $lesInformationsUser->fetch_assoc();
+
+    // echo "<pre>" . print_r($user, 1) . "</pre>";
+
+    //récupération infos des followers
+    $laQuestionEnSqlFollowers = "
+                SELECT users.*
+                FROM followers
+                LEFT JOIN users ON users.id = followers.following_user_id
+                WHERE followers.followed_user_id = '$userId'    
+                GROUP BY users.id
+                ";
+
+    $lesInformationsFollowers = $mysqli->query($laQuestionEnSqlFollowers);
+
+    /*
+    echo "<pre>";
+    print_r($lesInformationsFollowers->fetch_all(MYSQLI_ASSOC));
+    echo "</pre>";
+    */
+    ?>
+
+    <header>
+        <img src="resoc.jpg" alt="Logo de notre réseau social" />
+        <nav id="menu">
+            <a href="news.php">Actualités</a>
+            <a href="wall.php?user_id=<?php echo $user['id'] ?>">Mur</a>
+            <a href="feed.php?user_id=<?php echo $user['id'] ?>">Flux</a>
+            <a href="tags.php?tag_id=1">Mots-clés</a>
+        </nav>
+        <nav id="user">
+            <a href="#">Profil</a>
+            <ul>
+                <li><a href="settings.php?user_id=<?php echo $user['id'] ?>">Paramètres</a></li>
+                <li><a href="followers.php?user_id=<?php echo $user['id'] ?>">Mes suiveurs</a></li>
+                <li><a href="subscriptions.php?user_id=<?php echo $user['id'] ?>">Mes abonnements</a></li>
+            </ul>
+
+        </nav>
+    </header>
+
+    <div id="wrapper">
+        <aside>
+            <img src="user.jpg" alt="Portrait de l'utilisatrice" />
+            <section>
+                <h3>Présentation</h3>
+                <p>Sur cette page vous trouverez la liste des personnes qui
+                    suivent les messages de l'utilisatrice <?php echo $user['alias'] ?>
+                    n° <?php echo $user['id'] ?></p>
+            </section>
+        </aside>
+
+        <main class='contacts'>
+            <?php
+            // Etape 4: à vous de jouer
+                //@todo: faire la boucle while de parcours des abonnés et mettre les bonnes valeurs ci dessous 
+            if ($lesInformationsFollowers->num_rows >  0) {
+                while ($row = $lesInformationsFollowers->fetch_assoc()) {
+                    echo '<article>';
+                    echo '<img src="user.jpg" alt="blason"/>';
+                    echo '<h3>' . htmlspecialchars($row['alias']) . '</h3>';
+                    echo '<p>id:' . htmlspecialchars($row['id']) . '</p>';
+                    echo '</article>';
+                }
+            }
+            ?>
+        </main>
+    </div>
+</body>
+
 </html>
